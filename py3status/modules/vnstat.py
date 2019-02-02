0bfbd64cfdd49ae61cc45562eeb811a384bdb260
lasers <lasers@users.noreply.github.com>
2019-02-02 08:24:52 -0600

    vnstat: new module from scratch

--- py3status/modules/vnstat.py
+++ py3status/modules/vnstat.py
@@ -1,25 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Display vnstat statistics.
+Display vnStat traffic statistics.
 
 Configuration parameters:
     cache_timeout: refresh interval for this module (default 180)
-    format: display format for this module (default '{total}')
-    initial_multi: set to 1 to disable first bytes
-        (default 1024)
-    left_align: (default 0)
-    multiplier_top: if value is greater, divide it with unit_multi and get
-        next unit from units (default 1024)
-    precision: (default 1)
-    statistics_type: d for daily, m for monthly (default 'd')
-    thresholds: thresholds to use for color changes (default [])
-    unit_multi: value to divide if rate is greater than multiplier_top
-        (default 1024)
+    format: display format for this module
+        *(default "vnStat [\?color=good {daily_rx} {daily_rx_unit}] "
+        "[\?color=bad {daily_tx} {daily_tx_unit}] "
+        "[\?color=darkgray {daily_total} {daily_total_unit}]")*
+    thresholds:  specify color thresholds to use (default [])
 
 Format placeholders:
-    {down} download
-    {total} total
-    {up} upload
+    {version}           eg 1
+    {interface}         eg eno1
+    {daily_timestamp}   eg 2019-02-04
+    {daily_rx}          eg 954.6  # MiB
+    {daily_tx}          eg 58.2  # MiB
+    {daily_total}       eg 1.0  # GiB
+    {daily_avg}         eg 27.4  # KiB
+    {monthly_timestamp} eg 2019-02
+    {monthly_rx}        eg 15.0  # GiB
+    {monthly_tx}        eg 1.6  # GiB
+    {monthly_total}     eg 16.5  # GiB
+    {monthly_avg}       eg 58.4  # KiB
+    {all_time_rx}       eg 271.4  # GiB
+    {all_time_tx}       eg 16.7  # GiB
+    {all_time_total}    eg 288.0  # GiB
+
+    You can replicate placeholders with an unit (e.g., '{daily_total_unit}')
+    to display `KiB`, `MiB`, `GiB`, etc to be used with `{daily_total}`.
 
 Color thresholds:
     xxx: print a color based on the value of `xxx` placeholder
@@ -31,25 +40,89 @@ Examples:
 ```
 # colorize thresholds
 vnstat {
-    format = '[\?color=total {total}]'
+    format = '[\?color=daily_total {daily_total} {daily_total_unit}]'
     thresholds = [
         (838860800, "degraded"),  # 838860800 B -> 800 MiB
         (943718400, "bad"),       # 943718400 B -> 900 MiB
     ]
 }
+
+# show daily placeholders
+vnstat {
+    format = "vnStat: "
+    format += "[\?color=darkgray&show daily] "
+    format += "[\?color=white {daily_timestamp}] "
+    format += "[\?color=good {daily_rx} {daily_rx_unit}] "
+    format += "[\?color=bad {daily_tx} {daily_tx_unit}] "
+    format += "[\?color=degraded {daily_total} {daily_total_unit}] "
+    format += "[\?color=deepskyblue {daily_avg} {daily_avg_unit}]"
+}
+
+# show monthly placeholders
+vnstat {
+    format = "vnStat: "
+    format += "[\?color=darkgray&show monthly] "
+    format += "[\?color=white {monthly_timestamp}] "
+    format += "[\?color=good {monthly_rx} {monthly_rx_unit}] "
+    format += "[\?color=bad {monthly_tx} {monthly_tx_unit}] "
+    format += "[\?color=degraded {monthly_total} {monthly_total_unit}] "
+    format += "[\?color=deepskyblue {monthly_avg} {monthly_avg_unit}]"
+}
+
+# show all times placeholders
+vnstat {
+    format = "vnStat: "
+    format += "[\?color=darkgray&show all time] "
+    format += "[\?color=good {all_time_rx} {all_time_rx_unit}] "
+    format += "[\?color=bad {all_time_tx} {all_time_tx_unit}] "
+    format += "[\?color=degraded {all_time_total} {all_time_total_unit}]"
+}
+
+# show statisitcs image
+vnstat {
+    on_click 1 = "exec ~/.config/py3status/vnstati.sh"
+
+    # #!/usr/bin/env sh
+    # vnstati -i eno1 --fiveminutes -o /tmp/pystatus_vnstat-0.png
+    # vnstati -i eno1 -h -o /tmp/pystatus_vnstat-1.png
+    # vnstati -i eno1 -d -o /tmp/pystatus_vnstat-2.png
+    # vnstati -i eno1 -m -o /tmp/pystatus_vnstat-3.png
+    # vnstati -i eno1 --top -o /tmp/pystatus_vnstat-4.png
+    # convert -append /tmp/pystatus_vnstat* /tmp/py3status_vnstat.png
+    # rm /tmp/pystatus_vnstat-*.png
+    # xdg-open /tmp/py3status_vnstat.png
+}
 ```
 
-@author shadowprince
+@author shadowprince, lasers
 @license Eclipse Public License
 
 SAMPLE OUTPUT
-{'full_text': '826.4 mb'}
+[
+    {'full_text': 'vnStat '},
+    {'full_text': '978.1 MiB ', 'color': '#00FF00'},
+    {'full_text': '66.2 MiB ', 'color': '#FF0000'},
+    {'full_text': '1.0 GiB', 'color': '#A9A9A9'}
+]
+
+monthly
+[
+    {'full_text': 'vnStat '},
+    {'full_text': '15.1 GiB ', 'color': '#00FF00'},
+    {'full_text': '1.6 GiB ', 'color': '#FF0000'},
+    {'full_text': '16.7 GiB', 'color': '#A9A9A9'}
+]
+
+all_time
+[
+    {'full_text': 'vnStat '},
+    {'full_text': '271.5 GiB ', 'color': '#00FF00'},
+    {'full_text': '16.7 GiB ', 'color': '#FF0000'},
+    {'full_text': '288.2 GiB', 'color': '#A9A9A9'}
+]
 """
 
-from __future__ import division  # python2 compatibility
-
 STRING_NOT_INSTALLED = "not installed"
-STRING_INVALID_TYPE = "invalid statistics_type"
 
 
 class Py3status:
@@ -58,73 +131,88 @@ class Py3status:
 
     # available configuration parameters
     cache_timeout = 180
-    format = "{total}"
-    initial_multi = 1024
-    left_align = 0
-    multiplier_top = 1024
-    precision = 1
-    statistics_type = "d"
+    format = (
+        u"vnStat "
+        u"[\?color=good {daily_rx} {daily_rx_unit}] "
+        u"[\?color=bad {daily_tx} {daily_tx_unit}] "
+        u"[\?color=darkgray {daily_total} {daily_total_unit}]"
+    )
     thresholds = []
-    unit_multi = 1024
 
     def post_config_hook(self):
-        """
-        Format of total, up and down placeholders under FORMAT.
-        As default, substitutes left_align and precision as %s and %s
-        Placeholders:
-            value - value (float)
-            unit - unit (string)
-        """
         if not self.py3.check_commands("vnstat"):
             raise Exception(STRING_NOT_INSTALLED)
-        elif self.statistics_type not in ["d", "m"]:
-            raise Exception(STRING_INVALID_TYPE)
-        self.slice = slice(*(3, 6) if self.statistics_type == "d" else (8, 11))
-        self.value_format = "{value:%s.%sf} {unit}" % (self.left_align, self.precision)
-        # list of units, first one - value/initial_multi, second - value/1024,
-        # third - value/1024^2, etc...
-        self.units = ["kb", "mb", "gb", "tb"]
+
+        placeholders = self.py3.get_placeholders_list(self.format)
+        abbrs = ("_rx", "_tx", "_total", "_avg")
+        self.format = self.py3.update_placeholder_formats(
+            self.format, {x: ":g" for x in placeholders if x.endswith(abbrs)}
+        )
+        self.button_open = 1
+        self.placeholders = placeholders
+        self.keys = [
+            "version",
+            "interface",
+            "daily_timestamp",
+            "daily_rx",
+            "daily_tx",
+            "daily_total",
+            "daily_avg",
+            "monthly_timestamp",
+            "monthly_rx",
+            "monthly_tx",
+            "monthly_total",
+            "monthly_avg",
+            "all_time_rx",
+            "all_time_tx",
+            "all_time_total",
+        ]
 
         # deprecations
-        self.coloring = getattr(self, "coloring", None)
-        if self.coloring and not self.thresholds:
+        self.statistics_type = {"d": "daily_", "m": "monthly_"}.get(
+            getattr(self, "statistics_type", "d"), "daily_"
+        )
+        self.coloring = getattr(self, "coloring", {})
+        if self.coloring and "total" in placeholders and not self.thresholds:
             self.thresholds = [
                 (num * 1024 ** 2, col) for num, col in self.coloring.items()
             ]
+        else:
+            self.coloring = False
 
         self.thresholds_init = self.py3.get_color_names_list(self.format)
 
-    def _divide_and_format(self, value):
-        # Divide a value and return formatted string
-        value /= self.initial_multi
-        for i, unit in enumerate(self.units):
-            if value > self.multiplier_top:
-                value /= self.unit_multi
-            else:
-                break
-        return self.value_format.format(value=value, unit=unit)
+    def _get_vnstat_data(self):
+        lines = self.py3.command_output("vnstat --oneline b")
+        return dict(zip(self.keys, lines.splitlines()[0].split(";")))
 
     def vnstat(self):
-        vnstat_data = self.py3.command_output("vnstat --oneline b")
-        values = vnstat_data.splitlines()[0].split(";")[self.slice]
-        stat = dict(zip(["down", "up", "total"], map(int, values)))
-        response = {"cached_until": self.py3.time_in(self.cache_timeout)}
+        vnstat_data = self._get_vnstat_data()
 
+        # deprecations
+        color = None
+        for x, y in (("tx", "up"), ("rx", "down"), ("total", "total")):
+            vnstat_data[y] = vnstat_data[self.statistics_type + x]
         if self.coloring:
-            response["color"] = self.py3.threshold_get_color(stat["total"])
+            color = self.py3.threshold_get_color(vnstat_data["total"])
 
         for x in self.thresholds_init:
-            if x in stat:
-                self.py3.threshold_get_color(stat[x], x)
+            if x in vnstat_data:
+                self.py3.threshold_get_color(vnstat_data[x], x)
 
-        response["full_text"] = self.py3.safe_format(
-            self.format,
-            dict(
-                total=self._divide_and_format(stat["total"]),
-                up=self._divide_and_format(stat["up"]),
-                down=self._divide_and_format(stat["down"]),
-            ),
-        )
+        for x in list(self.placeholders):
+            try:
+                keys, value = [x, x + "_unit"], int(vnstat_data[x])
+                vnstat_data.update(zip(keys, self.py3.format_units(value)))
+            except (KeyError, ValueError):
+                self.placeholders.remove(x)
+
+        response = {
+            "cached_until": self.py3.time_in(self.cache_timeout),
+            "full_text": self.py3.safe_format(self.format, vnstat_data),
+        }
+        if color:
+            response["color"] = color
         return response
 
 
